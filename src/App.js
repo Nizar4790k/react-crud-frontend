28188553d951f696c4ab725a4da83d87d02d9ec4
Nizar4790k <nizar4790k@gmail.com>
2022-02-12 16:20:30 -0400

    Se agrego el login y el registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+
+import './App.css';
 function App() {
+
+  const  [user,setUser] = useState({});
+
+  const onLoadUser = (user)=>{
+    localStorage.setItem('user',JSON.stringify(user));
+    setUser(user);
+    
+  }
+
   return (
-    <div className="App">
-      <button type="button" class="btn btn-primary">Hello World </button>
+    <div>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Login onLoadUser={onLoadUser}/>}></Route>
+        <Route path="/Login" element={<Login onLoadUser={onLoadUser}/>}></Route>
+        <Route path="/Register" element={<Register/>} />
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
